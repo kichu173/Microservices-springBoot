cab05c9d1c23a96eec0632d7875d45dc6105af29
Kiran Kumar K <kichu173.kk@gmail.com>
2022-09-23 23:58:02 +0530

    Implemented API gateway and circuit breaker

--- OrderService/src/main/java/com/code/orderservice/external/client/PaymentService.java
+++ OrderService/src/main/java/com/code/orderservice/external/client/PaymentService.java
@@ -1,13 +1,22 @@
 package com.code.orderservice.external.client;
 
+import com.code.orderservice.exception.CustomException;
 import com.code.orderservice.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name= "external", fallbackMethod = "fallback")
 @FeignClient("PAYMENT-SERVICE/payment")
 public interface PaymentService {
     @PostMapping
     ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default void fallback(Exception e) {
+        throw new CustomException("Payment Service is not available",
+                "UNAVAILABLE",
+                500);
+    }
 }
